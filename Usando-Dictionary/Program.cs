0052e78d89f0803d65766be02579dc2befe902c2
andregf1 ADARK <andre.franca2@etec.sp.gov.br>
2020-08-10 08:05:33 -0300

    Usando o estilo de coleção dicionario para ler um arquivo e somar o total de pontos.

--- Usando-Dictionary/Program.cs
+++ Usando-Dictionary/Program.cs
@@ -1,9 +1,32 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
 
 namespace Usando_Dictionary {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            Dictionary<string, int> records = new Dictionary<string, int>();
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+            try {
+                using (StreamReader sr = File.OpenText(path)) {
+                    while (!sr.EndOfStream) {
+                        string[] line = sr.ReadLine().Split(',');
+                        string key = line[0];
+                        int value = (records.ContainsKey(key)) ? records[key] + int.Parse(line[1]) : int.Parse(line[1]);
+                        records[key] = value;                        
+                    }
+                    foreach (var item in records) {
+                        Console.WriteLine($"Name: {item.Key}, Total points: {item.Value}");
+
+                    }
+                }
+
+            }
+            catch(IOException e) {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
